0f843a41bba57dfcbd4be96db346bb7c657065d7
Vandercleison Sturaro <vstecnologia@gmail.com>
2021-05-05 09:24:10 -0400

    changed footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,10 +3,10 @@ const Footer = () => {
         <div>
             <footer className="footer mt-auto py-3 bg-dark">
                 <div className="container">
-                    <p className="text-light">App desenvolvido por 
+                    <p className="text-light">App desenvolvido por
                     <a href="https://www.linkedin.com/in/vandercleison-sturaro" target="_blank" rel="noreferrer"> Vandercleison Sturaro</a></p>
-                                        <p className="text-light"><strong>:: Semana Spring React ::   </strong>     
-         <br/>Evento promovido pela escola DevSuperior</p>
+                    <p className="text-light"><strong>:: Semana Spring React ::   </strong>
+                        <br /><a href="https://github.com/vsturaro" target="_blank" rel="noreferrer">GitHub</a></p>
                 </div>
             </footer>
         </div>
