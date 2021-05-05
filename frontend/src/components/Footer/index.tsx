914b4ab83821ad0164e2a4c60662b97699a021ee
Vandercleison Sturaro <vstecnologia@gmail.com>
2021-05-05 06:46:22 -0400

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/vsturaro" target="_blank" rel="noreferrer">Vandercleison</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/vandercleison-sturaro" target="_blank" rel="noreferrer">Linkedin</a></small></p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
