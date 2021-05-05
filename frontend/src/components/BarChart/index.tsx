806e940d6f0d64790c9b221a10006b29d4f497f8
Vandercleison Sturaro <vstecnologia@gmail.com>
2021-05-05 08:32:21 -0400

    repopulating data

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -12,7 +12,7 @@ const BarChart = () => {
 
     const mockData = {
         labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+            categories: ['João', 'Mário César', 'Kalline', 'Edson', 'Paulo']
         },
         series: [
             {
--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -2,7 +2,7 @@ const DataTable = () => {
     return (
         <div className="table-responsive">
             <table className="table table-striped table-sm">
-                <thead>
+                <thead  className="text-center">
                     <tr>
                         <th>Data</th>
                         <th>Vendedor</th>
@@ -11,55 +11,34 @@ const DataTable = () => {
                         <th>Valor</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody className="text-center">
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
+                        <td>João</td>
                         <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>27</td>
+                        <td>R$ 15017.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Kaline</td>
+                        <td>50</td>
+                        <td>33</td>
+                        <td>R$ 22044.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Edson</td>
+                        <td>24</td>
+                        <td>11</td>
+                        <td>R$ 15012.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Paulo</td>
+                        <td>41</td>
+                        <td>30</td>
+                        <td>R$ 25017.00</td>
                     </tr>
                 </tbody>
             </table>
