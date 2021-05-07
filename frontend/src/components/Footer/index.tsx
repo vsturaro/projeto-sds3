2a3715ecc11e318db174b6b45d3b7247671940ee
Vandercleison Sturaro <vstecnologia@gmail.com>
2021-05-07 07:50:21 -0400

    Home, navigation

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,10 +3,13 @@ const Footer = () => {
         <div>
             <footer className="footer mt-auto py-3 bg-dark">
                 <div className="container">
-                    <p className="text-light">App desenvolvido por
-                    <a href="https://www.linkedin.com/in/vandercleison-sturaro" target="_blank" rel="noreferrer"> Vandercleison Sturaro</a></p>
-                    <p className="text-light"><strong>:: Semana Spring React ::   </strong>
-                        <br /><a href="https://github.com/vsturaro" target="_blank" rel="noreferrer">GitHub</a></p>
+                    <p className="text-light">App desenvolvido por:&nbsp;
+                    <a href="https://www.linkedin.com/in/vandercleison-sturaro" target="_blank" rel="noreferrer">Vandercleison Sturaro</a>
+                    &nbsp; | &nbsp; <a href="https://github.com/vsturaro" target="_blank" rel="noreferrer">GitHub</a>
+                    </p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+
                 </div>
             </footer>
         </div>
